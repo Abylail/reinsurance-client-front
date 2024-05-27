fcd9614ffa60f6259ee4e875d04f67cc6c8f6885
Абылай <bazarbayev.abylay@gmail.com>
2024-05-27 17:49:57 +0500

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'shop',
       exec_mode: 'cluster',
-      port: '3003',
+      port: '3004',
       instances: 1, // Or a number of instances
       script: '.output/server/index.mjs'
     }
