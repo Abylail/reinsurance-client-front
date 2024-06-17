6f54940d906594629e27a21b78a611fe80a9fb43
Абылай <bazarbayev.abylay@gmail.com>
2024-06-18 01:16:00 +0500

    remove geoip

--- middleware/geoLocation.global.js
+++ middleware/geoLocation.global.js
@@ -13,23 +13,23 @@ export default defineNuxtRouteMiddleware(async () => {
         return;
     }
 
-    const GeoIp = await import('geoip-lite');
-
-    const { req } = useNuxtApp().ssrContext?.event.node;
-    let ip;
-    const forwarded = req.headers['x-forwarded-for'];
-    if (forwarded) ip = forwarded.split(',').pop();
-    else {
-        ip = req.connection.remoteAddress
-            || req.socket.remoteAddress
-            || (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    }
-
-    // Проверка на наличие локального IPv6 адреса и его замена на IPv4 адрес
-    if (!ip || ip === '::1') {
-        ip = '127.0.0.1';
-    }
-
-    const geo = GeoIp.lookup(ip);
-    authStore.setCity(geo?.city?.toLowerCase());
+    // const GeoIp = await useGeo();
+    //
+    // const { req } = useNuxtApp().ssrContext?.event.node;
+    // let ip;
+    // const forwarded = req.headers['x-forwarded-for'];
+    // if (forwarded) ip = forwarded.split(',').pop();
+    // else {
+    //     ip = req.connection.remoteAddress
+    //         || req.socket.remoteAddress
+    //         || (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    // }
+    //
+    // // Проверка на наличие локального IPv6 адреса и его замена на IPv4 адрес
+    // if (!ip || ip === '::1') {
+    //     ip = '127.0.0.1';
+    // }
+    //
+    // const geo = GeoIp.lookup(ip);
+    // authStore.setCity(geo?.city?.toLowerCase());
 });
