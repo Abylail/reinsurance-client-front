e64c4fed083dcb0707752a1abe53507819386d3c
Абылай <bazarbayev.abylay@gmail.com>
2024-06-22 16:47:30 +0500

    set true city

--- middleware/geoLocation.global.js
+++ middleware/geoLocation.global.js
@@ -15,7 +15,6 @@ export default defineNuxtRouteMiddleware(async () => {
             || req.socket.remoteAddress
             || (req.connection.socket ? req.connection.socket.remoteAddress : null);
     }
-    ip = "37.151.221.0"
 
     if (!ip || ip === '::1') ip = '127.0.0.1';
 
