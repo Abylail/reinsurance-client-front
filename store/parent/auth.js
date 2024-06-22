bf61256f121c879e1e068d13f740759cc68c0156
Абылай <bazarbayev.abylay@gmail.com>
2024-06-22 17:37:21 +0500

    head

--- store/parent/auth.js
+++ store/parent/auth.js
@@ -132,6 +132,7 @@ const actions = {
             return;
         }
 
+        if (process.server) return;
         await $fetch(`http://ip-api.com/json`)
             .then(response => {
                 if (response.status === "success") {
